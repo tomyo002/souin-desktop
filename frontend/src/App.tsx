3434f29c140e0e2f7141e0f2fedd9b9dd1bd9da0
tomyo002 <daret.tom@gmail.com>
2024-04-19 11:25:36 +0200

    fix(front): fix language

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,6 @@
-import { DisplayPage } from "./components/page";
-
+import { DisplayPage } from './components/page';
 
 function App() {
-  return (
-    <DisplayPage baseUrl="http://localhost:2019" name="souin" />
-  );
+  return <DisplayPage baseUrl="http://localhost:2019" name="souin" />;
 }
 export default App;
--- frontend/src/components/page/DisplayPage.tsx
+++ frontend/src/components/page/DisplayPage.tsx
@@ -17,13 +17,13 @@ export const DisplayPage: React.FC<PropsDisplayPage> = ({ baseUrl, name }) => (
           baseUrl={baseUrl}
           name="process_resident_memory_bytes"
           timeMilliSecond={1000}
-          title="Octets de la mémoire résidente"
+          title="process resident memory bytes"
         />
         <Display
           baseUrl={baseUrl}
           name="process_virtual_memory_bytes"
           timeMilliSecond={1000}
-          title="Octets de la mémoire virtuelle"
+          title="process virtual memory bytes"
         />
       </div>
       <div className="flex flex-col justify-center">
@@ -31,7 +31,7 @@ export const DisplayPage: React.FC<PropsDisplayPage> = ({ baseUrl, name }) => (
           baseUrl={baseUrl}
           name="go_gc_duration_seconds"
           timeMilliSecond={5000}
-          title="Go gc durée par secondes"
+          title="go gc duration seconds"
         />
       </div>
     </div>

6f11e9d987b3ee67b15555f8254be3734a2d4a73
tomyo002 <daret.tom@gmail.com>
2024-04-26 16:53:28 +0200

    fix(front): fix footer

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
 
 export const Footer: React.FC = () => (
-  <footer className="footer p-4 bg-neutral text-neutral-content justify-center">
-    <aside className="grid-flow-col">
-      <p>Copyright © 2024 - All right reserved</p>
-    </aside>
+  <footer className="footer-center p-4 bg-neutral text-neutral-content">
+    <p>Copyright © 2024 - All right reserved</p>
   </footer>
 );

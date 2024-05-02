fdfcca768d04f3cdefac4aabf8f11567b5721301
tomyo002 <daret.tom@gmail.com>
2024-05-02 14:33:42 +0200

    fix(front): fix props name

--- frontend/src/components/atomic/Link.tsx
+++ frontend/src/components/atomic/Link.tsx
@@ -2,11 +2,11 @@ import React, { ReactNode } from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { ClassName } from 'src/utils';
 
-type PropsLink = ClassName & {
+type LinkProps = ClassName & {
   to: string;
   children: ReactNode;
 };
 
-export const Link: React.FC<PropsLink> = ({ to, ...rest }) => (
+export const Link: React.FC<LinkProps> = ({ to, ...rest }) => (
   <RouterLink to={to} {...rest} />
 );

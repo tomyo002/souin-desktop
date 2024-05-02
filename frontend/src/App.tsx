1962514051ef7516c810a75afc60982acb005f6a
tomyo002 <daret.tom@gmail.com>
2024-05-02 10:45:26 +0200

    feat(front): install package router dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
 import { ChartPage } from './components/page';
 
 function App() {
-  return <ChartPage baseUrl="https://localhost:2019" name="souin" />;
+  return (
+    <Routes>
+      <Route
+        element={<ChartPage baseUrl="https://localhost:2019" name="souin" />}
+        path="/"
+      />
+    </Routes>
+  );
 }
 export default App;

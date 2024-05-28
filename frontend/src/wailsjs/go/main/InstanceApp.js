0a686dd24b4fa2ad7bdc327ba20464ca994751b8
tomyo002 <daret.tom@gmail.com>
2024-05-28 15:35:52 +0200

    fix(back): fix linters

--- frontend/src/wailsjs/go/main/InstanceApp.js
+++ frontend/src/wailsjs/go/main/InstanceApp.js
@@ -18,10 +18,6 @@ export function Get() {
   return window['go']['main']['InstanceApp']['Get']();
 }
 
-export function Init() {
-  return window['go']['main']['InstanceApp']['Init']();
-}
-
 export function Set(arg1) {
   return window['go']['main']['InstanceApp']['Set'](arg1);
 }
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"embed"
+	"log"
 
 	"github.com/wailsapp/wails/v2"
+	"github.com/wailsapp/wails/v2/pkg/logger"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
 )
@@ -11,32 +13,74 @@ import (
 //go:embed all:frontend/dist
 var assets embed.FS
 
-func main() {
-	// Create an instance of the app structure
-	app := NewApp()
-	instanceApp := NewInstanceApp()
-	instanceApp.Init()
+const (
+	DefaultWidth  = 1024
+	DefaultHeight = 768
+	DefaultMin    = 400
+	Red           = 27
+	Green         = 38
+	Blue          = 54
+	Alpha         = 1
+)
 
-	// Create application with options
+func main() {
+	app := NewApp()
+	instanceApp, _ := NewInstanceApp()
+
 	err := wails.Run(&options.App{
-		Title:  "Souin Desktop",
-		Width:  1024,
-		Height: 768,
+		Title:             "Souin Desktop",
+		Width:             DefaultWidth,
+		Height:            DefaultHeight,
+		DisableResize:     true,
+		Fullscreen:        true,
+		WindowStartState:  options.Maximised,
+		Frameless:         false,
+		MinWidth:          DefaultMin,
+		MinHeight:         DefaultMin,
+		MaxWidth:          DefaultWidth,
+		MaxHeight:         DefaultHeight,
+		StartHidden:       false,
+		HideWindowOnClose: true,
+		BackgroundColour:  &options.RGBA{R: Red, G: Green, B: Blue, A: Alpha},
+		AlwaysOnTop:       false,
 		AssetServer: &assetserver.Options{
-			Assets: assets,
+			Assets:     assets,
+			Handler:    nil,
+			Middleware: nil,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
-		OnStartup:        app.startup,
+		Menu:                             nil,
+		Logger:                           nil,
+		LogLevel:                         logger.DEBUG,
+		LogLevelProduction:               logger.ERROR,
+		OnStartup:                        app.startup,
+		OnDomReady:                       nil,
+		OnShutdown:                       nil,
+		OnBeforeClose:                    nil,
+		CSSDragProperty:                  "",
+		CSSDragValue:                     "",
+		EnableDefaultContextMenu:         false,
+		EnableFraudulentWebsiteDetection: false,
 		Bind: []interface{}{
 			app,
 			instanceApp,
 		},
+		ErrorFormatter: nil,
 		Debug: options.Debug{
 			OpenInspectorOnStartup: false,
 		},
+		SingleInstanceLock: &options.SingleInstanceLock{
+			UniqueId:               "e3984e08-28dc-4e3d-b70a-45e961589cdc",
+			OnSecondInstanceLaunch: nil,
+		},
+		Assets:        nil,
+		AssetsHandler: nil,
+		EnumBind:      nil,
+		Windows:       nil,
+		Mac:           nil,
+		Linux:         nil,
+		Experimental:  nil,
 	})
-
 	if err != nil {
-		println("Error:", err.Error())
+		log.Fatal(err)
 	}
 }

123dcfa2535c0e900d05ebce1d49d675dc80a50a
tomyo002 <daret.tom@gmail.com>
2024-06-11 16:19:42 +0200

    fix(front): fix

--- frontend/src/components/layout/Menu.tsx
+++ frontend/src/components/layout/Menu.tsx
@@ -1,14 +1,28 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { InstanceContext } from 'src/context';
-import { path } from 'src/utils';
+import { InstanceType, path } from 'src/utils';
 
 import { Button, ButtonOutline, H1, Icon } from '../atomic';
 
 export const Menu: React.FC = () => {
+  const [hoveredInstance, setHoveredInstance] = useState<InstanceType>();
   const { instances, setInstances, currentInstance, setCurrentInstance } =
     useContext(InstanceContext);
   const navigate = useNavigate();
+
+  let hideTimeout: NodeJS.Timeout;
+
+  const handleMouseEnter = (instance: InstanceType) => {
+    clearTimeout(hideTimeout);
+    setHoveredInstance(instance);
+  };
+
+  const handleMouseLeave = () => {
+    hideTimeout = setTimeout(() => {
+      setHoveredInstance(undefined);
+    }, 300);
+  };
 
   return (
     <div className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
@@ -16,33 +30,37 @@ export const Menu: React.FC = () => {
       <div className="flex-1">
         {instances.map(instance => (
           <div className="flex mb-4" key={instance.name}>
-            <div className='flex-1 indicator w-5'>
+            <div className="flex-1 indicator w-5">
               <Link className="flex-1" to={path.CHART}>
-                <Button
-                  className={`w-full ${currentInstance === instance && 'border-black'}`}
+                <button
+                  className={`btn w-full ${currentInstance === instance && 'border-black'}`}
                   onClick={() => {
                     setCurrentInstance(instance);
                   }}
+                  onMouseEnter={() => handleMouseEnter(instance)}
+                  onMouseLeave={() => handleMouseLeave()}
                 >
                   {instance.name} : {instance.baseUrl}
-                </Button>
+                </button>
               </Link>
               <div className="indicator-item">
-                <Button
-                  className="btn-circle btn-error border-white btn-xs"
-                  onClick={() => {
-                    setInstances(
-                      instances.filter(
-                        inst =>
-                          inst.name !== instance.name ||
-                          inst.baseUrl !== instance.baseUrl,
-                      ),
-                    );
-                    navigate(path.HOME);
-                  }}
-                >
-                  <Icon className="text-white h-4 w-4" name="cross" />
-                </Button>
+                {instance === hoveredInstance && (
+                  <Button
+                    className="btn-circle btn-error border-white btn-xs"
+                    onClick={() => {
+                      setInstances(
+                        instances.filter(
+                          inst =>
+                            inst.name !== instance.name ||
+                            inst.baseUrl !== instance.baseUrl,
+                        ),
+                      );
+                      navigate(path.HOME);
+                    }}
+                  >
+                    <Icon className="text-white h-4 w-4" name="cross" />
+                  </Button>
+                )}
               </div>
             </div>
           </div>
